87b84bcffa994b0e6137df8062528a55238896cd
dawidkeyser <dawidkeyser95@gmail.com>
2023-03-01 09:05:05 +1300

    Page structure created and routed

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,12 +1,13 @@
-import logo from '../assets/images/logo.svg';
-import main from '../assets/images/main.svg';
-import Wrapper from '../assets/wrappers/LandingPage'
+import main from '../assets/images/main-alternative.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
 	return (
 		<Wrapper>
 			<nav>
-				<img src={logo} alt="jobify" className="logo" />
+				<Logo />
 			</nav>
 			<div className="container page">
 				{/* info */}
@@ -19,7 +20,7 @@ const Landing = () => {
 						gastropub mustache. Hot chicken quinoa semiotics woke JOMO kale chips +1
 						hammock tonx listicle.
 					</p>
-					<button className="btn btn-hero">Login/Register</button>
+					<Link to='/register' className="btn btn-hero">Login/Register</Link>
 				</div>
 				<img src={main} alt="job hunt" className="img main-img" />
 			</div>
